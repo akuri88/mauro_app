1b1f7ed20561e0e3386f5657bcbe0ed5bb3ff616
Adrian <akuri88@gmail.com>
2011-10-03 21:18:42 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MauroApp::Application.config.secret_token = '85b8c9bf6e7cf95a8f08b89b7791be6df59dbe6cec8e87fdd5bbdd6ed46079223468b6435d12007031cef34b8ee2b2a77ddf33556d33cf91806524a63ef27168'
